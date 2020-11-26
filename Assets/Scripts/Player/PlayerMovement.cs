9455b5226b0f79f740767ffcfa50dc59fb408dce
Marc Sevilleja <marc@sevilayha.com>
2020-11-26 02:11:44 -0800

    Simple movement implemented

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovementInput();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void MovementInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
